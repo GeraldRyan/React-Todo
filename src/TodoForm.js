1cefdfa59fba70b7576e3bd42ce83b0df6f9b8a7
Webbuildermn <gerald.ryan40@yahoo.com>
2020-04-06 14:39:07 -0500

    This is going the path of the online tutorial

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -3,45 +3,78 @@ import React from 'react'
 
 class TodoForm extends React.Component
 {
- 
+
   constructor(props)
   {
     super(props);
-    this.state = 
-    // this.state = {
-    //   task: "",
-    //   id: "",
-    //   completed: false
-    // }
-    this.list = []
+    this.state ={}
+      // this.state = {
+      //   task: "",f
+      //   id: "",
+      //   completed: false
+      // }
+      // this.list = []
   }
 
-state = {
-  text:''
-}
+  state = {
+    text: ""
+  }
 
- handleClick = (event) => {
-  // console.log(event.target.value)
-  // console.log(document.getElementById("task").value)
-  this.state = [...this.state,{"task":document.getElementById("task").value, id:Date(), completed:false}]
-  
-  this.list = [...this.list,document.getElementById("task").value]
-  // console.log(this.list)
-  console.log(this.state)
-}
+  handleClick = (event) =>
+  {
+    // console.log(event.target.value)
+    // console.log(document.getElementById("task").value)
 
 
+    // this.state = [...this.state, { "task": document.getElementById("task").value, id: Date(), completed: false }]
 
-  render(props)
+    this.list = [...this.list, document.getElementById("task").value]
+    // console.log(this.list)
+    console.log(this.state)
+    console.log(this.list)
+  }
+
+  handleChange = (event) =>
+  {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+
+  handleSubmit = (e) =>
+  {
+    e.preventDefault()
+    this.props.onSubmit({
+      text: this.state.text,
+      id: Date.now(),
+      completed: false
+    })
+    this.setState({
+      text: ""
+    })  
+  }
+
+  render()
   {
     return (
 
       <div>
         <h1>I'm rendering something: </h1>
-        <input type="text" placeholder="enter your task" id='task'/>
-        <button type='submit' onClick={this.handleClick} >Submit</button>
-        <br />
-        <p>{this.props.todo.task}</p>
+        <form onSubmit={this.handleSubmit} action="">
+          <input name="text"
+            type="text"
+            onChange={this.handleChange}
+            placeholder="enter your task"
+            id='task'
+            value={this.state.text} />
+          <button onClick={this.handleSubmit}  >Submit</button>
+          <br/>
+          <br/>
+
+        </form>
+        {/* <p>{this.props.todo.task}</p> */}
       </div>
     )
   }
