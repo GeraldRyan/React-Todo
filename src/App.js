3a87805f0bb8113f6bf933c0503b277f9467c102
Webbuildermn <gerald.ryan40@yahoo.com>
2020-04-06 14:54:01 -0500

    just committing

--- src/App.js
+++ src/App.js
@@ -4,49 +4,42 @@ import TodoList from './TodoList'
 import './App.css'
 
 
-class App extends React.Component // I am confused because in the sc i didn't see a constructor function
-{
-  constructor()
-  {
+class App extends React.Component {
+  constructor(){
     super()
-    // this.todo = [{
-    //   task: "Remove all the trees",
-    //   id: Date(),
-    //   completed: false
-    // }]
-
+    this.todo=[{
+      task:"Remove all the trees",
+      id:444,
+      completed: false
+    }]
   }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
 
-
   state = {
     count: 0
   }
 
-  increment = () =>
-  {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
+increment = () =>{
+  this.setState({
+    count: this.state.count +1
+  })
+}
 
-  increment = () =>
-  {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }
+increment = () =>{
+  this.setState({
+    count: this.state.count -1
+  })
+}
 
-  render()
-  {
+  render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList></TodoList>
-        {/* <TodoForm todo={this.todo[0]}></TodoForm> */}
+        <div></div>
       </div>
     );
   }
