28cf08ad09ee6d13a1eebed5dedd7b71ebf8de73
Webbuildermn <gerald.ryan40@yahoo.com>
2020-04-06 20:47:11 -0500

    just tidy slightly

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -25,6 +25,7 @@ export default class TodoList extends React.Component
   {
     return (
       <div>
+        <h2>:: My Tasks ::</h2>
       {this.props.tasks.map(todo => (
           <Todo id={todo.id} task={todo.task} />
         ))}
